4da335d5afc7fe8f0611429428f00c6327775754
MOHIT YADAV <127754053+GitMohit-9621@users.noreply.github.com>
2024-04-09 18:14:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Joke from './Componets/Joke';
+function App(props) {
+  
+
+  return (
+    <div className="App-joke">
+  
+ <Joke/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Button.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Button = (props) => {
+    return <button onClick={props.callApi}className="btn1">
+        Rendom Joke
+    </button>;
+}
+export default Button
